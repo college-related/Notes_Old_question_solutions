3540c6a090791033d82f58a329125f4e1e976471
Alson33 <be2018se607@gces.edu.np>
2020-07-20 13:50:04 +0545

    2018 more code question

---
+++ 3rd-Semester/JAVA/Old_Questions/2018-spring/UpperCaseDemo.java
@@ -0,0 +1,40 @@
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+// import java.awt.Color;
+
+public class UpperCaseDemo implements ActionListener {
+	
+	JTextField tf;
+	
+	UpperCaseDemo(){
+		JFrame f = new JFrame();
+		f.setSize(800, 800);
+		f.setVisible(true);
+		
+		JButton btn = new JButton("Action");
+		tf = new JTextField("Hello World");
+		
+		tf.setBounds(100, 50, 60, 20);
+		btn.setBounds(100, 100, 30, 40);
+		
+		btn.addActionListener(this);
+		
+		f.add(tf);
+		f.add(btn);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String str = tf.getText().toUpperCase();
+		tf.setText(str);
+		tf.setBackground(Color.BLUE);
+	}
+
+	public static void main(String[] args) {
+		new UpperCaseDemo();
+	}
+}
