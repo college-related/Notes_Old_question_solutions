86172481ff26d97e130e068ec17788940ddefe5f
Alson <42911859+Alson33@users.noreply.github.com>
2021-01-03 13:01:31 +0545

    Update SumAndDiffDemo.java

--- 3rd-Semester/JAVA/Old_Questions/2018-fall/SumAndDiffDemo.java
+++ 3rd-Semester/JAVA/Old_Questions/2018-fall/SumAndDiffDemo.java
@@ -1,27 +1,26 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class SumAndDiffDemo implements ActionListener {
     
-    TextField tf1, tf2, tf3;
-    Button addBtn, subBtn;
+    JTextField tf1, tf2, tf3;
+    JButton addBtn, subBtn;
 
     SumAndDiffDemo(){
-        Frame f = new Frame();
+        JFrame f = new JFrame();
 
             f.setSize(1000, 1000);
             f.setVisible(true);
 
-        tf1 = new TextField("10");
-        tf2 = new TextField("5");
-        tf3 = new TextField("");
+        tf1 = new JTextField("10");
+        tf2 = new JTextField("5");
+        tf3 = new JTextField("");
         tf1.setBounds(50, 50, 100, 30);
         tf2.setBounds(50, 100, 100, 30);
         tf3.setBounds(50, 150, 100, 30);
 
-        addBtn = new Button("Add");
-        subBtn = new Button("Sub");
+        addBtn = new JButton("Add");
+        subBtn = new JButton("Sub");
         addBtn.setBounds(50, 200, 50, 20);
         subBtn.setBounds(50, 250, 50, 20);
 
